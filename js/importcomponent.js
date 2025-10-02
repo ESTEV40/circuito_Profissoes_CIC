450cce90942ae2942f3142b1d04dc6c26273841a
Estêvão <sousaestevao17@gmail.com>
2025-10-02 02:51:04 -0300

    add: algoritmo de importação da modularização de components

---
+++ js/importcomponent.js
@@ -0,0 +1,42 @@
+const elements = document.querySelectorAll("import")
+
+async function fetchComponent(relative, path) {
+	const response = await fetch(path)
+	const text = await response.text()
+	return text
+}
+
+async function main() {
+	for (let element of elements) {
+		const path = element.getAttribute("path")
+		const relative = element.getAttribute("relative-path")
+		let importedElementString = await fetchComponent(relative, path)
+
+		let doc = new DOMParser().parseFromString(
+			importedElementString,
+			"text/html"
+		)
+
+		for (let links of doc.querySelectorAll("[href]")) {
+			let href = links.getAttribute("href")
+			if (href.startsWith("/")) {
+				let sliced = href.slice(1)
+				let newHref = relative + sliced
+				links.setAttribute("href", newHref)
+			}
+		}
+		for (let links of doc.querySelectorAll("[src]")) {
+			let src = links.getAttribute("src")
+			if (src.startsWith("/")) {
+				let sliced = src.slice(1)
+				let newSrc = relative + sliced
+				links.setAttribute("src", newSrc)
+			}
+		}
+
+		element.insertAdjacentHTML("afterend", doc.documentElement.innerHTML)
+		element.remove()
+	}
+}
+
+main()
